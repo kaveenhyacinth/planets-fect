1afa25e3367849ed48930dff3c5c297b14f865a8
kaveenhyacinth <alwiskaveen@gmail.com>
2025-01-01 08:28:37 +0530

    chore: configure scss and tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: {enabled: true},
+
+  css: ['~/assets/scss/main.scss'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "~/assets/scss/_colors.scss" as *;
+            @use "~/assets/scss/_typography.scss" as *;
+          `
+        }
+      }
+    }
+  }
 })
